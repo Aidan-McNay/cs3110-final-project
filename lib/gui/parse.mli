6eb00b162e9642786af088af5a3b152c73635c06
Aidan <acm289@cornell.edu>
2024-05-15 17:09:51 -0400

    Remove parsing
     - Remove string input parsing (since input is now from clicking, not from a string input)

--- lib/gui/parse.mli
+++
@@ -1,7 +0,0 @@
-(* @author Aidan McNay (acm289) *)
-
-exception Invalid_input of string
-(** Raised if a user's input isn't in a valid form for a chess move. *)
-
-val get_locs : string -> Utils.Location.t * Utils.Location.t
-(** [get_locs input] gets the two indicated locations based on [input]. *)
